6128d1551c40b5b2a8a08438da6dd524264294a5
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-14 03:06:54 -0300

    Criar tela de login

---
+++ src/pages/signin/Index.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-page class="flex flex-center column justify-center q-px-md">
+
+    <div class="full-width column items-center justify-center">
+      <q-icon name="fas fa-chevron-left"
+       size="23px"
+       class="absolute-left q-ma-lg"
+       color="grey"></q-icon>
+      <q-img class="logo q-mb-lg" src="../../../src/assets/instadev-logo.svg"></q-img>
+
+      <q-input filled v-model="email" label="E-mail" class="full-width q-mb-md" />
+      <q-input filled v-model="password" label="Password" type="password" class="full-width" />
+
+      <div class="column items-end full-width">
+        <a class="q-mt-md link" href="">Forgot password?</a>
+      </div>
+
+      <q-btn
+        color="primary"
+        label="Log in"
+        class="sign-in-button full-width q-mt-lg"
+      />
+
+      <div class="flex full-width row items-center justify-center q-mt-xl">
+        <q-img class="facebook-icon" src="../../../src/assets/facebook-logo.svg"></q-img>
+        <a class="link q-ml-xs" href="">Log in with Facebook</a>
+      </div>
+
+      <div class="full-width row items-center justify-center q-mt-xl">
+        <q-separator class="separator" inset />
+        <span class="text-black-opacity">OR</span>
+        <q-separator class="separator full-width q-mt-xl" inset />
+      </div>
+
+      <div class="full-width row items-center justify-center">
+        <span class="text-black-opacity">Don’t have an account?</span> <a class="q-ml-xs link" href="">Sign up.</a>
+      </div>
+    </div>
+    <div class="full-width column items-center absolute-bottom">
+      <q-separato class="full-width" />
+      <div class="q-my-lg">
+        <span class="text-black-opacity">Instagram ot Facebook</span>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 49px;
+  width: 182px;
+}
+.sign-in-button {
+  height: 44px;
+  border-radius: 5px;
+  background-color: $instablue;
+  opacity: 60%;
+}
+.facebook-icon {
+  height: 17px;
+  width: 17px;
+}
+.separator {
+  width: 350%;
+}
+.seta {
+  height: 17px;
+  width: 17px;
+}
+</style>
