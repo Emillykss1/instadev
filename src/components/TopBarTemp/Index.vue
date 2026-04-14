30ce7131b2765ec3ea9ddbf92a9d5c8b0a10df03
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-14 16:33:03 -0300

    criação da página main

---
+++ src/components/TopBarTemp/Index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="full-width row justify-between">
+  <q-img class="q-ml-sm" src="../../assets/camera.svg" width="28px"></q-img>
+  <q-img class="logo" src="../../assets/instadev-logo.svg"></q-img>
+  <q-img class="q-mr-sm" src="../../assets/curtidas.svg" width="28px"></q-img>
+</div>
+  <h1>oiii</h1>
+</template>
+
+<script>
+export default {
+  name: 'TopBarTemp'
+};
+</script>
+<style lang="scss" scoped>
+.logo{
+  width: 115px;
+  height: 30px;
+}
+.q-img{
+  cursor: pointer;
+}
+</style>
---
+++ src/pages/Main/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="full-width">
+    <TopBarTemp class="q-mb-lg"/>
+    <q-separetor class="full-width"/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import TopBarTemp from 'src/components/TopBarTemp/Index.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    TopBarTemp
+  },
+};
+</script>
