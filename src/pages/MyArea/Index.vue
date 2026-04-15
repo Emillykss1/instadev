be0193034d2c0eadaae3b1d0be09cc9f8d5d57ee
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-15 03:17:22 -0300

    feat: Adiciona Bottombar na tela MyArea

--- src/pages/MyArea/Index.vue
+++ src/pages/MyArea/Index.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page class="flex">
-   <div class="column q-px-md full-width"> 
-    <q-icon name="las la-bars" size="31px" class="absolute-right q-ma-xl"></q-icon>
+  <q-page class="flex column">
+   <div class="column q-pt-xl q-px-md full-width"> 
+    <q-icon name="las la-bars" size="31px" class="absolute-right q-ma-lg"></q-icon>
 
     <div class="full-width row items-center justify-center">
       <q-icon name="fas fa-lock" color="black" size="11px" class="q-mr-xs" />
@@ -9,7 +9,7 @@
       <q-icon name="fas fa-chevron-down" color="black" size="11px" class="q-ml-xs" />
     </div>
 
-    <div class="row items-center justify-between full-width">
+    <div class="row items-center justify-between full-width q-mt-lg">
       <div class="profile-border-neutral">
         <q-avatar size="96px" class="avatar-profile">
           <img class="avatar" src="https://cdn.quasar.dev/img/avatar.png" />
@@ -32,7 +32,7 @@
       </div>
     </div>
 
-    <div class="column">
+    <div class="column q-mt-md">
       <strong>Jacob West</strong>
       <span>
         Digital goodies designer @pixsellz
@@ -46,7 +46,7 @@
       color="white"
       flat
       dense
-      class="btn-edit full-width"
+      class="btn-edit full-width q-mt-md"
       text-color="black"
       label="Edit Profile"
     />
@@ -71,12 +71,46 @@
       </div>
     </div>
    </div> 
+   <q-separetor class="q-mt-md"/>
+   <div class="full-width">
+    <q-tabs v-model="tab" class="text-teal" active-color="grey-8">
+        <q-tab name="grid"> 
+            <q-img src="../../assets/grid.svg" width="22px"/>
+        </q-tab>
+        <q-tab name="profile"> 
+            <q-img src="../../assets/profile.svg" width="22px"/>
+        </q-tab>
+      </q-tabs>
+
+        <div class="row q-mb-xl">
+            <q-img
+            v-for="item in 5" :key="item"
+            class="cursor-pointer col-4"
+            :ratio="1"
+            src="https://picsum.photos/500/300"
+            />
+        </div>
+   </div>
+   <div class="container-bottom full-width q-pb-lg">
+      <q-separetor class="full-width q-mb-sm"/>
+    <BottomBar/>
+    </div>
   </q-page>
 </template>
 
 <script>
+import BottomBar from 'src/components/BottomBar/Index.vue';
+
 export default {
-  name: 'MyArea'
+  name: 'MyArea',
+  data(){
+    return{
+        tab: 'grid',
+    };
+  },
+  components: {
+    BottomBar
+  },
 };
 </script>
 
@@ -109,6 +143,6 @@ export default {
   height: 35px;
   border-radius: 6px;
   text-transform: none;
-  border: 2px solid rgba(60, 60, 67, 0.18);
+  border: 1px solid rgba(60, 60, 67, 0.18);
 }
 </style>
