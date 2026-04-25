1cd1a65725f498a677ab40674dec02ddffa4880d
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-25 18:53:46 -0300

    feat: adiciona like por id

--- src/components/Posts/Index.vue
+++ src/components/Posts/Index.vue
@@ -1,88 +1,117 @@
 <template>
   <div class="full-width q-pb-xl">
-      <div class="column full-width" v-for="item in items" :key="item.id">
-    <div class="row justify-between items-center full-width">
-      <div class="row items-center">
-        <q-avatar size="28px" class="q-mx-md">
+    <div class="column full-width" v-for="item in items" :key="item.id">
+      <div class="row justify-between items-center full-width">
+        <div class="row items-center">
+          <q-avatar size="28px" class="q-mx-md">
             <img src="https://cdn.quasar.dev/img/avatar.png" />
-        </q-avatar>
-        <div class="column">
-          <strong>{{item.user.user_name}}</strong>
-          <span>Tokyo, Japa</span>
+          </q-avatar>
+          <div class="column">
+            <strong>{{ item.user.user_name }}</strong>
+            <span>Tokyo, Japa</span>
+          </div>
         </div>
+        <q-icon class="q-mr-md" name="fas fa-ellipsis-h" size="15px" color="dark-items" />
       </div>
-      <q-icon class="q-mr-md" name="fas fa-ellipsis-h" size="15px" color="dark-items"></q-icon>
-    </div>
-    <q-img class="q-mt-sm cursor-pointer"
-          :src="item.image" 
-          :ratio="1" 
-          @click="countClicks++"
+
+      <q-img
+        class="q-mt-sm cursor-pointer"
+        :src="item.image"
+        :ratio="1"
+        @dblclick="handleLike(item.id)"
       >
-      <q-icon
-        v-if="animationClass != ''"
-        :class="animationClass"
-        class="absolute-center"
-        name="fas fa-heart"
-        color="white"
-      />
+        <q-icon
+          v-if="animatedPostId === item.id"
+          class="absolute-center animate-like"
+          name="fas fa-heart"
+          color="white"
+        />
       </q-img>
 
-    <div class="row justify-between q-mx-sm q-my-md">
-    <div>    
-        <q-img class="q-mr-md" width="25px"  src="../../assets/curtidas.svg" ></q-img>
-        <q-img class="q-mr-md" width="22px" src="../../assets/comentarios.svg" ></q-img>
-        <q-img class="q-mr-md" width="25px" src="../../assets/direct.svg"></q-img>             
-    </div>         
-    <q-icon class="q-mr-xl" name="fas fa-ellipsis-h" size="20px" color="dark-items"></q-icon> 
-    <q-img width="20px" src="../../assets/salvar.svg"></q-img>       
-    </div> 
-    <div class="row items-center q-mb-xs" v-if="item.number_likes">
-      <q-avatar size="17px" class="q-ml-sm q-mr-xs">
-            <img src="https://cdn.quasar.dev/img/avatar.png" />
-      </q-avatar>
-      Liked by <strong>{{ item.user.user_name }}</strong> and <strong>{{ item.number_likes }} others</strong>
-    </div>
-    <div class="q-mx-sm q-mb-lg">
-      <strong>{{ item.user.user_name }}</strong> {{ item.description }}
-    </div>
+      <div class="row justify-between q-mx-sm q-my-md">
+        <div>
+          <q-img
+            class="q-mr-md cursor-pointer"
+            width="25px"
+            src="../../assets/curtidas.svg"
+            @click="handleLike(item.id)"
+          />
+          <q-img class="q-mr-md" width="22px" src="../../assets/comentarios.svg" />
+          <q-img class="q-mr-md" width="25px" src="../../assets/direct.svg" />
+        </div>
+
+        <q-icon class="q-mr-xl" name="fas fa-ellipsis-h" size="20px" color="dark-items" />
+        <q-img width="20px" src="../../assets/salvar.svg" />
+      </div>
+
+      <div class="row items-center q-mb-xs" v-if="likesOf(item) > 0">
+        <q-avatar size="17px" class="q-ml-sm q-mr-xs">
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+        </q-avatar>
+        Liked by <strong>{{ item.user.user_name }}</strong> and
+        <strong>{{ likesOf(item) }} others</strong>
+      </div>
+
+      <div class="q-mx-sm q-mb-lg">
+        <strong>{{ item.user.user_name }}</strong> {{ item.description }}
+      </div>
     </div>
   </div>
 </template>
 
-<script >
-export default{
+<script>
+export default {
   name: 'PostsPage',
   props: ['items'],
-  data(){
-    return{
-    animationClass: '',
-    countClicks: 0
-   };
-  },
-  watch: {
-    countClicks(){
-      setTimeout(() => {
-        if(this.countClicks != 2){
-        this.countClicks = 0;
-       }
-      }, 1000);
 
-      
-      if(this.countClicks == 2){
-        this.addLikeInPost();
-        this.countClicks = 0
-      }
+  data() {
+    return {
+      animatedPostId: null,
+      likedPosts: {},
+      likesCount: {},
+    }
+  },
+
+  watch: {
+    items: {
+      immediate: true,
+      handler(newItems) {
+        newItems.forEach((item) => {
+          if (this.likesCount[item.id] === undefined) {
+            this.likesCount[item.id] = Number(item.number_likes) || 0
+          }
+        })
+      },
     },
   },
+
   methods: {
-    addLikeInPost(){
-      this.animationClass = 'animate-like';
-      setTimeout(() =>{
-      this.animationClass = '';
-    }, 600);
+    likesOf(item) {
+      if (this.likesCount[item.id] !== undefined) {
+      return this.likesCount[item.id]
+    }
+
+      return Number(item.number_likes) || 0
+    },
+
+    handleLike(postId) {
+      this.animatedPostId = postId
+
+      setTimeout(() => {
+        if (this.animatedPostId === postId) {
+          this.animatedPostId = null
+        }
+      }, 600)
+
+      if (this.likedPosts[postId]) {
+        return
+      }
+
+      this.likedPosts[postId] = true
+      this.likesCount[postId] = (this.likesCount[postId] || 0) + 1
+    },
   },
-},
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -90,22 +119,22 @@ export default{
   animation-duration: 500ms;
   animation-name: liked;
 }
-@keyframes liked{
-    from{
-      font-size: 45px;
-    }
-    10%{
-      font-size: 48px;
-    }
-    75%{
-      font-size: 75px;
-    }
-    90%{
-      font-size: 75px;
-    }
-    to{
-      font-size: 0px;
-    }
-}
 
+@keyframes liked {
+  from {
+    font-size: 45px;
+  }
+  10% {
+    font-size: 48px;
+  }
+  75% {
+    font-size: 75px;
+  }
+  90% {
+    font-size: 75px;
+  }
+  to {
+    font-size: 0px;
+  }
+}
 </style>
---
+++ src/stores/like-post.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
+import { useAuthStore } from 'src/stores/auth-store'
+
+export const useMyPostsStore = defineStore('my-posts', {
+  state: () => ({
+    posts: [],
+  }),
+
+  actions: {
+    async AddLikeInPost(postId) {
+      const authStore = useAuthStore()
+
+      const { data } = await api.get(`/posts/add-like/${postId}`, {
+        headers: {
+          Authorization: `Bearer ${authStore.token}`,
+        },
+      })
+
+
+      return data
+    },
+  },
+})
