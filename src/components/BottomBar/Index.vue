2a1dd378f2ff4fe10f4b4dbe55858612c937b1d6
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-25 00:39:01 -0300

    feat: adiciona methods rota

--- src/components/BottomBar/Index.vue
+++ src/components/BottomBar/Index.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="bottom-bar full-width row justify-around items-center">
-  <q-img  src="../../assets/house.svg" width="32px"></q-img>
+  <q-img  src="../../assets/house.svg" width="32px" @click="goTo('main')"></q-img>
   <q-img  src="../../assets/reels.svg" width="32px"></q-img>
   <q-img class="enviar"  src="../../assets/direct.svg" ></q-img>
   <q-img  src="../../assets/pesquisas.svg" width="34px"></q-img>
-  <q-avatar size="29px">
+  <q-avatar size="29px" class="cursor-point" @click="goTo('my-area')">
       <img src="https://cdn.quasar.dev/img/avatar.png" />
   </q-avatar>
 
@@ -13,7 +13,12 @@
 
 <script>
 export default {
-  name: 'BottomBar'
+  name: 'BottomBar',
+  methods: {
+    goTo(route){
+      this.$router.push({path: route});
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
