3ff370158bdb06858676848db42f88cdee93c0e8
Emilly Kaiane <kkaianee20@gmail.com>
2026-04-25 18:46:34 -0300

    feat: exibe username do usuário no MenuDrawer com dados do store

--- src/components/MenuDrawer/Index.vue
+++ src/components/MenuDrawer/Index.vue
@@ -9,55 +9,65 @@
   >
     <div class="container-items">
       <div class="q-pl-md q-pt-xl">
-       <span>s.khasanov_</span>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/archive.svg"/>
-         <span>Archive</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/activity.svg"/>
-         <span>Your Activity</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/nametag.svg"/>
-         <span>Nametag</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/salvar.svg"/>
-         <span>Saved</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/close-friends.svg"/>
-         <span>Close Friends</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/discover.svg"/>
-         <span>Discover People</span>
-       </div>
-       <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/open-facebook.svg"/>
-         <span>Open Facebook</span>
-       </div>
+        <span>{{ user.user_name || 'usuario' }}</span>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/archive.svg" />
+          <span>Archive</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/activity.svg" />
+          <span>Your Activity</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/nametag.svg" />
+          <span>Nametag</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/salvar.svg" />
+          <span>Saved</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/close-friends.svg" />
+          <span>Close Friends</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/discover.svg" />
+          <span>Discover People</span>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-img width="24px" class="q-mr-sm" src="../../assets/open-facebook.svg" />
+          <span>Open Facebook</span>
+        </div>
       </div>
+
       <div class="q-pa-sm">
         <div class="q-mt-lg">
-         <q-img width="24px" class="q-mr-sm" src="../../assets/settings.svg"/>
-         <span>Settings</span>
-       </div>
+          <q-img width="24px" class="q-mr-sm" src="../../assets/settings.svg" />
+          <span>Settings</span>
+        </div>
       </div>
     </div>
   </q-drawer>
 </template>
 
 <script>
+import { useUserStore } from 'src/stores/user-store'
+
 export default {
   name: 'MenuDrawer',
 
   props: {
     drawerRight: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
 
   emits: ['update:drawerRight'],
@@ -69,9 +79,28 @@ export default {
       },
       set(value) {
         this.$emit('update:drawerRight', value)
+      },
+    },
+
+    user() {
+      const userStore = useUserStore()
+      return userStore.user || {}
+    },
+  },
+
+  async mounted() {
+    await this.loadProfileData()
+  },
+
+  methods: {
+    async loadProfileData() {
+      const userStore = useUserStore()
+
+      if (!userStore.user || !userStore.user.id) {
+        await userStore.getUserData()
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
